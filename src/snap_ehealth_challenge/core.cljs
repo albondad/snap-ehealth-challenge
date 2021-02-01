ab73c01addea93a06cc5183f41b7449355cf2b27
Al Bondad <albondad@gmail.com>
2021-01-31 23:18:19 -0600

    Add add task feature

--- src/snap_ehealth_challenge/core.cljs
+++ src/snap_ehealth_challenge/core.cljs
@@ -5,53 +5,33 @@
 
 
 ;; state variables
+(def empty-array [])
 (def tasks (r/atom []))
-(def task-list-controls-input-value (r/atom "test"))
+(def task-list-controls-input-value (r/atom ""))
+
+
+;; utility functions
+(defn create-id []
+  (js/Date.now))
+
 
 ;; event handlers
-
-(def handle-task-controls-button-on-click
-  (fn [] 
-    (reset! tasks (concat @tasks [{:value @task-list-controls-input-value}]))
+(defn handle-task-controls-button-on-click []
+    (reset! tasks (concat @tasks [{:id (create-id) :checked false :value @task-list-controls-input-value}]))
     (println @tasks)
-    (reset! task-list-controls-input-value "") 
-    ))
+    (reset! task-list-controls-input-value ""))
 
-(def handle-task-list-controls-input-on-change
-  (fn [event] (reset! task-list-controls-input-value event.target.value)))
+(defn handle-task-list-controls-input-on-change []
+  [event] (reset! task-list-controls-input-value event.target.value))
+
 
 ;; components
-
-
-; "
-;   0:  id
-;   1:  name
-;   3:  age
-; ; "
-; (def items (r/atom [
-;   ["joe" "27"]
-;   ["" "35"]
-;   ["bill" "42"]]))
-
-; (println (get @items 1))
-; (println (get (get @items 1) 1))
-; (println (js/Date.now))
-
-; (defn itemsComponent []
-;   [:div
-;     (for [item @items]
-;       [:div {:key (get item 0)} (get item 1)])])
-
-; (def handle-add-button-on-click
-;   (fn [] (reset! click-count (+ @click-count 1))))
-
-; (def handle-input-on-change
-;   (fn [event] 
-;     (reset! items (concat @items [[(js/Date.now) (js/Date.now) 9]])) 
-;     (println @input-value)))
-
 (defn application []
-  [container [task-list handle-task-list-controls-input-on-change handle-task-controls-button-on-click]])
+  [container [task-list 
+    @tasks 
+    handle-task-list-controls-input-on-change 
+    handle-task-controls-button-on-click
+    handle-task-list-list-item-checkbox-on-click]])
 
 (defn container [body]
   [:div
@@ -59,37 +39,28 @@
     [:div {:class "ContainerHeader"} 
       [:div {:class "ContainerHeaderSection"}
         [:div {:class "ContainerHeaderSectionPrimary"} "Snap eHealth"]
-        [:div {:class "ContainerHeaderSectionSecondary"} "Task Checklist"]
-        ]
+        [:div {:class "ContainerHeaderSectionSecondary"} "Task Checklist"]]
       [:div {:class "ContainerHeaderSection"}
         [:div {:class "ContainerHeaderSectionPrimary"} "January 01, 2020"]
-        [:div {:class "ContainerHeaderSectionSecondary"} "12:00 PM"]
-        ]
-      ]
-    [:div {:class "ContainerBody"} body]
-    ])
+        [:div {:class "ContainerHeaderSectionSecondary"} "12:00 PM"]]]
+    [:div {:class "ContainerBody"} body]])
 
-(defn task-list [handle-task-list-controls-input-on-change handle-task-controls-button-on-click]
+(defn task-list 
+  [tasks 
+   handle-task-list-controls-input-on-change handle-task-controls-button-on-click 
+   handle-task-list-list-item-checkbox-on-click]
   [:div {:class "TaskList"} 
     [:div {:class "TaskListControls"}
-      [:input {:class "TaskListControlsInput" :value @task-list-controls-input-value :on-change handle-task-list-controls-input-on-change}]
-      [:button {:class "TaskListControlsButton" :on-click handle-task-controls-button-on-click} "Add"]
-      ]
-    ])
-
-
-; (defn button []
-;   [:button {:class "button" :on-click handle-add-button-on-click} "add"])
-
-; (defn input [event]
-;   [:input {:placeholder "test" :on-change handle-input-on-change}])
-
-; (defn counter [value]
-;   [:div value])
+      [:input {:class "TaskListControlsInput" :value @task-list-controls-input-value :placeholder "task name" :on-change handle-task-list-controls-input-on-change}]
+      [:button {:class "TaskListControlsButton" :on-click handle-task-controls-button-on-click} "Add"]]
+  [:div {:class "TaskLisList"} (for [task tasks]
+    [:div {:key (get task :id) :class "TaskListListItem"} 
+      [:div {:class (+ "TaskListListItemCheckbox" (if (get task :checked) " TaskListListItemCheckbox--Checked" ""))} ""]
+      [:div {:class "TaskListListItemValue"} (get task :value)]
+      [:div {:class "TaskListListItemDelete"} "delete"]])]])
 
 
 ;; initialize app
-
 (defn mount-root []
   (d/render [application] (.getElementById js/document "app")))
 
